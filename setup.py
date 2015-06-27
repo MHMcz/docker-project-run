97f1b3d66df5b730c603c761a5bffee822fa5925
Jan Markup <mhmcze@gmail.com>
2015-06-27 12:37:11 +0200

    add license

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python
+# Copyright © 2014 Jan Markup <mhmcze@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
 from distutils.core import setup
 import os
 import shutil
